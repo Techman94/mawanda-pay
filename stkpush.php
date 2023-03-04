15d6f27c76c473797a354fc7df863947ebd2c7c4
Techman <tykemaster94@gmail.com>
2023-03-04 08:36:19 +0300

    adding stk push files

--- stkpush.php
+++ stkpush.php
@@ -3,17 +3,17 @@
 include 'accessToken.php';
 date_default_timezone_set('Africa/Nairobi');
 $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-$callbackurl = 'https://umeskiasoftwares.com/darajaapp/callback.php';
+$callbackurl = 'https://mawandaorganics.com/payments/callback.php';
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 $BusinessShortCode = '174379';
 $Timestamp = date('YmdHis');
 // ENCRIPT  DATA TO GET PASSWORD
 $Password = base64_encode($BusinessShortCode . $passkey . $Timestamp);
-$phone = '254768168060';
+$phone = '254727888751';
 $money = '1';
 $PartyA = $phone;
 $PartyB = '254708374149';
-$AccountReference = 'UMESKIA SOFTWARES';
+$AccountReference = 'AZTECH SOFTWARES';
 $TransactionDesc = 'stkpush test';
 $Amount = $money;
 $stkpushheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
@@ -43,6 +43,7 @@ curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 $curl_response = curl_exec($curl);
 //ECHO  RESPONSE
+
 $data = json_decode($curl_response);
 $CheckoutRequestID = $data->CheckoutRequestID;
 $ResponseCode = $data->ResponseCode;
